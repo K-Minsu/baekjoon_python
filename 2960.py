ed5ccb7776bea129b8a5ceaea6288810b3fdd321
K-Minsu <minsue9608@naver.com>
2025-02-12 17:14:29 +0900

    에라토스테네스의 체

---
+++ 2960.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+N, K = map(int, sys.stdin.readline().split())
+
+is_prime = [True] * (N + 1)
+is_prime[0] = is_prime[1] = False
+
+cnt = 0
+ans = 0
+
+for i in range(2, N + 1):   # 제곱근까지만 반복
+    for j in range(i, N + 1, i):
+        if is_prime[j]:
+            is_prime[j] = False
+            cnt += 1
+
+            if cnt == K:
+                ans = j
+
+print(ans)
