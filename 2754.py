505f204d2d7ff6c14a422e9a76e2f903e69ad2ae
K-Minsu <minsue9608@naver.com>
2025-02-17 23:31:26 +0900

    학점계산

---
+++ 2754.py
@@ -0,0 +1,9 @@
+import sys
+
+C_score = sys.stdin.readline().strip()
+
+score_list = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D+', 'D0', 'D-', 'F']
+gpa_list = [4.3, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0.0]
+
+if C_score in score_list:
+    print(gpa_list[score_list.index(C_score)])
