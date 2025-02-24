2ca5967e7067a3c7cd1aa1be7d763d8820f394a6
K-Minsu <minsue9608@naver.com>
2025-02-24 22:07:17 +0900

    팰린드롬인지 확인하기

---
+++ 10988.py
@@ -0,0 +1,17 @@
+import sys
+
+word = sys.stdin.readline().strip()
+
+if len(word) % 2 == 0:
+    if word[:len(word) // 2] == word[:len(word) // 2 - 1:-1]:
+        print(1)
+
+    else:
+        print(0)
+
+else:
+    if word[:len(word) // 2] == word[:len(word) // 2:-1]:
+        print(1)
+    
+    else:
+        print(0)
